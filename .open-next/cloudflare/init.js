af9daba10a25f272dabb9f23957f26196b9a750e
srichandra321 <srichandra321@gmail.com>
2025-04-17 11:32:19 +0000

    firebase first commit

--- .open-next/cloudflare/init.js
+++ .open-next/cloudflare/init.js
@@ -48,7 +48,7 @@ function initRuntime() {
   };
   Object.assign(globalThis, {
     Request: CustomRequest,
-    __BUILD_TIMESTAMP_MS__: 1744712418483,
+    __BUILD_TIMESTAMP_MS__: 1744889198072,
     __NEXT_BASE_PATH__: ""
   });
 }
